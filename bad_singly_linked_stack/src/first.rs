18ba4510188b25b04b6a647ee678a75fd1763e81
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-02-29 14:33:19 -0500

    Made naive linked list usable with Box

--- bad_singly_linked_stack/src/first.rs
+++ bad_singly_linked_stack/src/first.rs
@@ -1,5 +1,20 @@
 /// Naive linked list wont compile
-pub enum List<T> {
-    Empty,
-    Element(T, List<T>),
+#[derive(Debug)]
+pub enum Node<T> {
+    Nil,
+    Construct(T, Box<Node<T>>),
+}
+impl<T> Node<T> {
+    pub fn cons(element: T, next: Box<Self>) -> Box<Self> {
+        return Box::new(Node::Construct(element, next));
+    }
+    pub fn nil() -> Box<Self> {
+        return Box::new(Node::Nil);
+    }
+}
+
+#[test]
+fn list() {
+    let list = Node::cons(0, Node::cons(1, Node::cons(2, Node::nil())));
+    println!("{:?}", list);
 }
