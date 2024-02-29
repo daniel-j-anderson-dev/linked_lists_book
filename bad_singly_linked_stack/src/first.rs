be83745accf0f5d60cda5a569b9553fa7404ae65
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-02-29 14:50:00 -0500

    Type aliased List

--- bad_singly_linked_stack/src/first.rs
+++ bad_singly_linked_stack/src/first.rs
@@ -1,14 +1,16 @@
 /// Naive linked list
+pub type Next<T> = Option<Box<Node<T>>>;
+
 #[derive(Debug)]
 pub struct Node<T> {
     element: T,
-    next: Option<Box<Node<T>>>,
+    next: Next<T>
 }
 impl<T> Node<T> {
-    pub fn cons(element: T, next: Option<Box<Self>>) -> Option<Box<Self>> {
-        return Some(Box::new(Node(element, next)));
+    pub fn cons(element: T, next: Next<T>) -> Next<T> {
+        return Some(Box::new(Node { element, next }));
     }
-    pub fn nil() -> Option<Box<Self>> {
+    pub fn nil() -> Next<T> {
         return None;
     }
 }
